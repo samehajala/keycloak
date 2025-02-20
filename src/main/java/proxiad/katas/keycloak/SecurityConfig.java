ccbbd00d9272b3e31db76920a6b5da572afb9839
samehajala <sameh.ajala@etudiant-enit.utm.tn>
2025-02-20 11:13:35 +0100

    jwt auth converter

--- src/main/java/proxiad/katas/keycloak/SecurityConfig.java
+++ src/main/java/proxiad/katas/keycloak/SecurityConfig.java
@@ -9,15 +9,22 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthConverter jwtAuthConverter ; 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf().disable().authorizeRequests().anyRequest().authenticated() ; 
-        http.oauth2ResourceServer().jwt() ; 
+
+        http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthConverter);
+
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) ; 
+        
         return http.build() ; 
     }
 }
